3dc588555bea74eb154e45d1660f502bfee51056
Pavel Timofeev <pavel.timofeev@software.dell.com>
2016-01-18 17:22:21 +0300

    publish update at 2016-01-18

--- publish.cmd
+++ publish.cmd
@@ -1,8 +1,14 @@
 @echo off
 REM 
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+set today=%MyDate:~0,4%-%MyDate:~4,2%-%MyDate:~6,2%
+echo "publish update at %today%"
+
+git status
+pause
 git add .
 git status
-git commit -m "publish update"
+git commit -m "publish update at %today%"
 pause
 git push
 pause
