259f75f4cb7fc6bd1ec8a6147097e2ba9703d848
Pavel Timofeev <pavel.timofeev@software.dell.com>
2016-01-18 16:52:35 +0300

    publish update

---
+++ _site/publish.cmd
@@ -0,0 +1,8 @@
+@echo off
+REM 
+git add .
+git status
+git commit -m "publish update"
+rem git push
+
+pause
--- _site/run.cmd
+++ _site/run.cmd
@@ -1,1 +1,3 @@
+@echo off
+REM server address will displayed at runtime, usually it http://localhost:4000
 jekyll serve --watch
